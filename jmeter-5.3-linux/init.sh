7527cd702fe9de7fbbf9a3aae59e566713c704ea
广深小龙 <qq772262624@163.com>
2022-09-10 12:59:27 +0800

    update init.sh

--- jmeter-5.3-linux/init.sh
+++ jmeter-5.3-linux/init.sh
@@ -4,4 +4,4 @@ chmod 777 jmeter-5.3-linux/ant/bin/ant && chmod 777 ./build && chmod 777 ./build
 
 # 1、如果遇到Jenkins提示无权限，则：chmod 777 jmeter-5.3-linux/*
 
-# 2、运行：./build env,key,add_param:value1,${BUILD_URL}/InterfaceReport,x.jmx
+# 2、运行参数：./build env,key,add_param:value1,${BUILD_URL}/InterfaceReport,x.jmx
