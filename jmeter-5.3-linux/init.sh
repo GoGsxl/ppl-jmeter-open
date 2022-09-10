c8a518ce73c31a29b96c39e6affcbdb211fc5fc2
广深小龙 <qq772262624@163.com>
2022-09-10 13:05:31 +0800

    update init.sh

--- jmeter-5.3-linux/init.sh
+++ jmeter-5.3-linux/init.sh
@@ -1,7 +1,7 @@
-chmod 777 jmeter-5.3-linux/ant/bin/ant && chmod 777 ./build && chmod 777 ./build-docker && chmod 777 jmeter-5.3-linux/lib/ext/JMeterPluginsCMD.sh
+chmod 777 jmeter-5.3-linux/ant/bin/ant && chmod 777 ./build && chmod 777 ./build-docker && chmod 777 jmeter-5.3-linux/lib/ext/JMeterPluginsCMD.sh && chmod 777 jmeter-5.3-linux/*
 
 # Jenkins 持续集成(上述ant、build需先授权777权限)
 
 # 1、如果遇到Jenkins提示无权限，则：chmod 777 jmeter-5.3-linux/*
 
-# 2、运行参数：./build env,key,add_param:value1,${BUILD_URL}/InterfaceReport,x.jmx
+# 2、运行：./build env,key,add_param:value1,${BUILD_URL}/InterfaceReport,x.jmx
